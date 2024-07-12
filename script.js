0595a842512eb4adc301a88f2e4ef898bd526621
blackCAT14-CLOUD <hafizinovus@gmail.com>
2024-07-12 23:12:47 +0200

    refractor for birth

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc")
+const sname = document.getElementById("name")
+
+
+
+title.innerText = "Ma Maman, Mon Cœur, Ma Vie 💓";
+
+desc.innerText = "Je sais que ces derniers mois n'ont pas été faciles pour toi, mais je ne peux pas te dire à quel point je t'aime. Ces moments difficiles ont montré à quel point tu es forte et inspirante. J'espère ne jamais te perdre et que notre lien devienne encore plus fort et plus profond.";
+
+sname.innerText = "Grâce 😊";
